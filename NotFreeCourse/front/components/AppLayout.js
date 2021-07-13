d1b3cab5cb38451a374dd3073c7da0c23105af35
Lee Jaeseong <fg9658@gmail.com>
2021-07-13 12:54:29 +0900

    commit backup

--- NotFreeCourse/front/components/AppLayout.js
+++ NotFreeCourse/front/components/AppLayout.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Menu, Input } from 'antd'; 
 
-const AppLayout = () => {
-
+const AppLayout = ({ children }) => {
+    return (
+        <div>
+            <Menu mode="horizontal">
+                <Menu.Item key="home">
+                    노드버드
+                </Menu.Item>
+                <Menu.Item key="profile">
+                    프로필
+                </Menu.Item>
+                <Menu.Item key="main">
+                    <Input.Search  enterButton />
+                </Menu.Item>
+            </Menu>
+            {children}
+        </div>
+    )
 };
 
 export default AppLayout;
